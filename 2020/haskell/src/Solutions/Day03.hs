b382d3213592584f886e1ef58b04d9fd0f319023
Andrea Tupini <tupini07@gmail.com>
2020-12-13 22:08:50 -0600

    fixed day 3 part 1

--- 2020/haskell/src/Solutions/Day03.hs
+++ 2020/haskell/src/Solutions/Day03.hs
@@ -11,21 +11,24 @@ import           Text.Parsec.Char
 
 -- input
 
-type Input = [S.Set Int]
+type Input = (Int, [S.Set Int])
 
 parseInput :: String -> Input
-parseInput = map (S.fromList . findIndices (`elem` "#")) . lines
-
+parseInput i = (colLen, sets)
+  where
+    lns = lines i
+    sets = map (S.fromList . findIndices (`elem` "#")) $ lns
+    colLen = length . head $ lns
 -- solution
 
 part1 :: Input -> Int
-part1 inpt = countTrue $ map isTreePos (generatePath 0 0)
+part1 (colLen, inpt) = countTrue $ map isTreePos (generatePath 0 0)
   where
     mmx = length inpt
+    isTreePos (r, c) = S.member c (inpt !! r)
     generatePath r c = (r,c) : if r >= (mmx - 1)
                                   then []
-                                  else generatePath (r+1) ((c+3) `mod` 11)
-    isTreePos (r, c) = S.member c (inpt !! r)
+                                  else generatePath (r+1) ((c+3) `mod` colLen)
 
 part2 :: Input -> String
 part2 = undefined
