c0e3db8e330b2242b21589cc37066e703dec5df6
Andrea Tupini <tupini07@gmail.com>
2022-12-10 14:36:41 -0600

    start progress on day 5

--- 2022/elixir/lib/advent_of_code/day_05.ex
+++ 2022/elixir/lib/advent_of_code/day_05.ex
@@ -1,5 +1,25 @@
 defmodule AdventOfCode.Day05 do
-  def part1(_args) do
+  def part1(args) do
+    [starting_structure, _instructions] = String.split(args, "\n\n")
+
+    [column_line | starting_structure] =
+      starting_structure
+      |> String.split("\n")
+      |> Enum.reverse()
+
+    columns = column_line |> String.trim() |> String.split("   ") |> Enum.count()
+
+    structure =
+      1..columns
+      |> Enum.reduce(%{}, fn col, acc ->
+        Map.put(acc, col, [])
+      end)
+
+    for {line, row} <- Enum.with_index(starting_structure) do
+      IO.puts("Processing line #{row}: #{line}")
+    end
+
+    columns
   end
 
   def part2(_args) do
