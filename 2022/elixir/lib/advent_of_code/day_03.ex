6d56a4c76631e033e88fc383ffee1bfe4e9eb431
Andrea Tupini <tupini07@gmail.com>
2022-12-10 13:51:52 -0600

    day 4

--- 2022/elixir/lib/advent_of_code/day_03.ex
+++ 2022/elixir/lib/advent_of_code/day_03.ex
@@ -1,7 +1,56 @@
 defmodule AdventOfCode.Day03 do
-  def part1(args) do
+  @docp """
+  To help prioritize item rearrangement, every item type can be converted to a
+  priority:
+  - Lowercase item types a through z have priorities 1 through 26.
+  - Uppercase item types A through Z have priorities 27 through 52.
+  """
+  defp priority(char) do
+    case char do
+      char when char in ?a..?z -> char - ?a + 1
+      char when char in ?A..?Z -> char - ?A + 27
+    end
   end
 
-  def part2(_args) do
+  def part1(args) do
+    args
+    |> String.split("\n")
+    |> Enum.map(fn contents ->
+      len_contents = String.length(contents)
+      charlist = String.to_charlist(contents)
+
+      # index of middle
+      middle_idx = div(len_contents, 2)
+
+      # part 1 contains the first half of the list
+      part_1_set = MapSet.new(Enum.take(charlist, middle_idx))
+
+      # part 2 contains the second half of the list
+      part_2_set = MapSet.new(Enum.drop(charlist, middle_idx))
+
+      # the intersection of the two sets is the list of characters that are in both
+      # parts of the list
+      intersection = MapSet.intersection(part_1_set, part_2_set)
+
+      intersection
+    end)
+    |> Enum.flat_map(fn intersection -> Enum.map(intersection, &priority(&1)) end)
+    |> Enum.sum()
+  end
+
+  def part2(args) do
+    args
+    |> String.split("\n")
+    |> Enum.chunk_every(3)
+    |> Enum.map(fn group_rucksacks ->
+      # each group of 3 rucksacks is a list of 3 lists of characters
+      # we need to convert each list of characters into a set of characters
+      # and then find the intersection of the 3 sets
+      group_rucksacks
+      |> Enum.map(fn rucksack -> MapSet.new(String.to_charlist(rucksack)) end)
+      |> Enum.reduce(fn rucksack, acc -> MapSet.intersection(rucksack, acc) end)
+    end)
+    |> Enum.flat_map(fn intersection -> Enum.map(intersection, &priority(&1)) end)
+    |> Enum.sum()
   end
 end
--- 2022/elixir/lib/advent_of_code/day_04.ex
+++ 2022/elixir/lib/advent_of_code/day_04.ex
@@ -1,7 +1,39 @@
 defmodule AdventOfCode.Day04 do
-  def part1(_args) do
+  def part1(args) do
+    args
+    |> String.split("\n")
+    |> Enum.filter(&(&1 != ""))
+    |> Enum.map(fn pair ->
+      [left_range, right_range] = String.split(pair, ",")
+
+      [left_a, left_b] = String.split(left_range, "-") |> Enum.map(&String.to_integer/1)
+      [right_a, right_b] = String.split(right_range, "-") |> Enum.map(&String.to_integer/1)
+
+      is_left_in_right = left_a >= right_a and left_b <= right_b
+      is_right_in_left = right_a >= left_a and right_b <= left_b
+
+      is_left_in_right or is_right_in_left
+    end)
+    |> Enum.filter(& &1)
+    |> Enum.count()
   end
 
-  def part2(_args) do
+  def part2(args) do
+    args
+    |> String.split("\n")
+    |> Enum.filter(&(&1 != ""))
+    |> Enum.map(fn pair ->
+      [left_range, right_range] = String.split(pair, ",")
+
+      [left_a, left_b] = String.split(left_range, "-") |> Enum.map(&String.to_integer/1)
+      [right_a, right_b] = String.split(right_range, "-") |> Enum.map(&String.to_integer/1)
+
+      (left_a >= right_a and left_a <= right_b) or
+        (left_b >= right_a and left_b <= right_b) or
+        (right_a >= left_a and right_a <= left_b) or
+        (right_b >= left_a and right_b <= left_b)
+    end)
+    |> Enum.filter(& &1)
+    |> Enum.count()
   end
 end
